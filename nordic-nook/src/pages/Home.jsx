ac80cb81cdcbb536a56389eb388a278178e6327c
Claudia Barbosa <claara0002@gmail.com>
2025-08-24 16:43:48 +0200

    chore: add Tailwind setup and test styles on Home page

--- nordic-nook/src/pages/Home.jsx
+++ nordic-nook/src/pages/Home.jsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <section>
       <h2>Home</h2>
-      <p>Välkommen till Nordic Nook </p>
+      <p class="text-3xl font-bold underline">Välkommen till Nordic Nook </p>
     </section>
   );
 }
